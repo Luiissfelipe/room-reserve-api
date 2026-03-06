1cf0f1e7977b1a1176cadf86b1464bffb364adad
Luís Felipe Morais <149626850+Luiissfelipe@users.noreply.github.com>
2026-03-06 13:53:16 -0300

    Initial commit

---
+++ src/main/java/com/desafio/room_reserve_api/RoomReserveApiApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.room_reserve_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReserveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReserveApiApplication.class, args);
+	}
+
+}
