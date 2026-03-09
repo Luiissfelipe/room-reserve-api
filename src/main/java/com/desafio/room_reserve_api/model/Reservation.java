72fbb4d914aadde583863bf6ff62df77181cfa15
Luís Felipe Morais <149626850+Luiissfelipe@users.noreply.github.com>
2026-03-09 08:32:38 -0300

    feat: Implement Reservation and ReservationStatus entity with JPA mapping

---
+++ src/main/java/com/desafio/room_reserve_api/model/Reservation.java
@@ -0,0 +1,74 @@
+package com.desafio.room_reserve_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@EntityListeners(AuditingEntityListener.class)
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @NotBlank
+    private String purpose;
+
+    @NotNull
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @NotNull
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "reservation_status", nullable = false)
+    private ReservationStatus reservationStatus;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @CreatedDate
+    @Column(name = "creation_date", updatable = false)
+    private LocalDateTime creationDate;
+
+    @LastModifiedDate
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+
+    private boolean isPeriodValid() {
+        return this.startDate.isBefore(this.endDate);
+    }
+
+    public void cancel() {
+        if (this.reservationStatus == ReservationStatus.CANCELLED) {
+            return;
+        }
+
+        if (LocalDateTime.now().isAfter(this.startDate) || LocalDateTime.now().isEqual(this.startDate)) {
+            throw new IllegalStateException("Não é possível cancelar uma reserva que já iniciou ou já passou.");
+        }
+
+        this.reservationStatus = ReservationStatus.CANCELLED;
+    }
+}
---
+++ src/main/java/com/desafio/room_reserve_api/model/ReservationStatus.java
@@ -0,0 +1,7 @@
+package com.desafio.room_reserve_api.model;
+
+public enum ReservationStatus {
+    CONFIRMED,
+    CANCELLED,
+    COMPLETED
+}
