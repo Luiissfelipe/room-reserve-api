ea407a5217093581bc3af37b7f259b7a471b4773
Luís Felipe Morais <149626850+Luiissfelipe@users.noreply.github.com>
2026-03-10 13:30:29 -0300

    fix: enable JPA auditing for entity lifecycle management

--- src/main/java/com/desafio/room_reserve_api/RoomReserveApiApplication.java
+++ src/main/java/com/desafio/room_reserve_api/RoomReserveApiApplication.java
@@ -2,8 +2,10 @@ package com.desafio.room_reserve_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class RoomReserveApiApplication {
 
 	public static void main(String[] args) {
